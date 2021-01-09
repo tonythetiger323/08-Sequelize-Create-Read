db6b11ee9675b5a2cd5774b9f62d16311d629ae2
Tony Greeley <tonygreeley@Tonys-iMac.local>
2021-01-09 10:24:28 -0600

    Created .env file and added environment variables and installed dotenv dependency and updated config.js.

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PW,
+    "database": process.env.MYSQL_DB,
+    "host": process.env.MYSQL_HOST,
+    "port": 3306,
     "dialect": "mysql"
   },
   "test": {
